7ce879df0c3a9ab8bcbe8804d0841182fdcce1e4
Pavol Juhas <pavol.juhas@gmail.com>
2012-09-13 21:56:41 +0000

    Added convenience file for running unit tests.

---
+++ diffpy/utils/tests/run.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# diffpy.utils      by DANSE Diffraction group
+#                   Simon J. L. Billinge
+#                   (c) 2012 Trustees of the Columbia University
+#                   in the City of New York.  All rights reserved.
+#
+# File coded by:    Pavol Juhas
+#
+# See AUTHORS.txt for a list of people who contributed.
+# See LICENSE.txt for license information.
+#
+##############################################################################
+
+"""Convenience module for executing all unit tests with
+
+python -m diffpy.utils.tests.run
+"""
+
+# version
+__id__ = '$Id$'
+
+
+if __name__ == '__main__':
+    import sys
+    from diffpy.utils.tests import test
+    # produce zero exit code for a successful test
+    sys.exit(not test().wasSuccessful())
+
+# End of file
