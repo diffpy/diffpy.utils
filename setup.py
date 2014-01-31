7b1b574460077f181939f56626163d02bc7fe20c
Pavol Juhas <pavol.juhas@gmail.com>
2014-01-31 14:06:07 -0500

    Make setup importable.
    Only run it if it is the main script.  The versiondata from setup.py
    can thus be imported from sphinx conf.py.

--- setup.py
+++ setup.py
@@ -10,13 +10,15 @@ from setuptools import setup, find_packages
 
 # versioncfgfile holds version data for git commit hash and date.
 # It must reside in the same directory as version.py.
-versioncfgfile = 'diffpy/utils/version.cfg'
+MYDIR = os.path.dirname(os.path.abspath(__file__))
+versioncfgfile = os.path.join(MYDIR, 'diffpy/utils/version.cfg')
 
 def gitinfo():
     from subprocess import Popen, PIPE
-    proc = Popen(['git', 'describe'], stdout=PIPE)
+    kw = dict(stdout=PIPE, cwd=MYDIR)
+    proc = Popen(['git', 'describe'], **kw)
     desc = proc.stdout.read()
-    proc = Popen(['git', 'log', '-1', '--format=%H %ai'], stdout=PIPE)
+    proc = Popen(['git', 'log', '-1', '--format=%H %ai'], **kw)
     glog = proc.stdout.read()
     rv = {}
     rv['version'] = '-'.join(desc.strip().split('-')[:2])
@@ -29,7 +31,8 @@ def getversioncfg():
     from ConfigParser import SafeConfigParser
     cp = SafeConfigParser()
     cp.read(versioncfgfile)
-    if not os.path.isdir('.git'):  return cp
+    gitdir = os.path.join(MYDIR, '.git')
+    if not os.path.isdir(gitdir):  return cp
     d = cp.defaults()
     g = gitinfo()
     if g['version'] != d.get('version') or g['commit'] != d.get('commit'):
@@ -39,12 +42,12 @@ def getversioncfg():
         cp.write(open(versioncfgfile, 'w'))
     return cp
 
-cp = getversioncfg()
+versiondata = getversioncfg()
 
 # define distribution
-setup(
+setup_args = dict(
         name = "diffpy.utils",
-        version = cp.get('DEFAULT', 'version'),
+        version = versiondata.get('DEFAULT', 'version'),
         namespace_packages = ['diffpy'],
         packages = find_packages(),
         test_suite = 'diffpy.utils.tests',
@@ -71,5 +74,8 @@ setup(
             'Topic :: Scientific/Engineering :: Physics',
         ],
 )
+
+if __name__ == '__main__':
+    setup(**setup_args)
 
 # End of file
