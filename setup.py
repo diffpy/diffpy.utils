afb666c8193c5fa3b0fe8bb4fd178a3410b78e84
Justin Calamari <justin.calamari@gmail.com>
2015-07-27 19:26:39 -0400

    Manually checked all code and fixed any found errors.

--- setup.py
+++ setup.py
@@ -92,8 +92,7 @@ setup_args = dict(
             'Operating System :: Microsoft :: Windows',
             'Operating System :: POSIX',
             'Operating System :: Unix',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.x'
             'Topic :: Scientific/Engineering :: Physics',
         ],
 )
