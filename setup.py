d4d41dadc6d91c1675361496d9b7a3738140db2c
Pavol Juhas <pavol.juhas@gmail.com>
2016-03-17 12:32:00 -0400

    Move package files to the src directory.
    Avoid overriding normal installation when python is started
    from the source directory.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ FALLBACK_VERSION = '1.2.1.post0'
 # versioncfgfile holds version data for git commit hash and date.
 # It must reside in the same directory as version.py.
 MYDIR = os.path.dirname(os.path.abspath(__file__))
-versioncfgfile = os.path.join(MYDIR, 'diffpy/utils/version.cfg')
+versioncfgfile = os.path.join(MYDIR, 'src/diffpy/utils/version.cfg')
 gitarchivecfgfile = versioncfgfile.replace('version.cfg', 'gitarchive.cfg')
 
 def gitinfo():
@@ -72,7 +72,8 @@ setup_args = dict(
         name = "diffpy.utils",
         version = versiondata.get('DEFAULT', 'version'),
         namespace_packages = ['diffpy'],
-        packages = find_packages(),
+        packages = find_packages('src'),
+        package_dir = {'' : 'src'},
         test_suite = 'diffpy.utils.tests',
         include_package_data = True,
         zip_safe = False,
