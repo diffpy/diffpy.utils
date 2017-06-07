diffpy/diffpy.srfit

Remove excessive indentation of setup_args.
No change in code function.

setup.py
@@ -71,39 +71,39 @@ versiondata = getversioncfg()
 
 # define distribution
 setup_args = dict(
-        name = "diffpy.srfit",
-        version = versiondata.get('DEFAULT', 'version'),
-        namespace_packages = ['diffpy'],
-        packages = find_packages('src'),
-        package_dir = {'' : 'src'},
-        test_suite = 'diffpy.srfit.tests',
-        include_package_data = True,
-        install_requires = [],
-        zip_safe = False,
-        author = "Simon J.L. Billinge",
-        author_email = "sb2896@columbia.edu",
-        maintainer = "Pavol Juhas",
-        maintainer_email = "pavol.juhas@gmail.com",
-        description = "SrFit - Structure refinement from diffraction data",
-        license = 'BSD-style license',
-        url = "https://github.com/diffpy/diffpy.srfit",
-        keywords = "optimization constraints restraints structure refinement complex modeling",
-        classifiers = [
-            # List of possible values at
-            # http://pypi.python.org/pypi?:action=list_classifiers
-            'Development Status :: 5 - Production/Stable',
-            'Environment :: Console',
-            'Intended Audience :: Developers',
-            'Intended Audience :: Education',
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: BSD License',
-            'Operating System :: MacOS',
-            'Operating System :: POSIX',
-            'Programming Language :: Python :: 2.7',
-            'Topic :: Scientific/Engineering :: Chemistry',
-            'Topic :: Scientific/Engineering :: Physics',
-            'Topic :: Software Development :: Libraries',
-        ],
+    name = "diffpy.srfit",
+    version = versiondata.get('DEFAULT', 'version'),
+    namespace_packages = ['diffpy'],
+    packages = find_packages('src'),
+    package_dir = {'' : 'src'},
+    test_suite = 'diffpy.srfit.tests',
+    include_package_data = True,
+    install_requires = [],
+    zip_safe = False,
+    author = "Simon J.L. Billinge",
+    author_email = "sb2896@columbia.edu",
+    maintainer = "Pavol Juhas",
+    maintainer_email = "pavol.juhas@gmail.com",
+    description = "SrFit - Structure refinement from diffraction data",
+    license = 'BSD-style license',
+    url = "https://github.com/diffpy/diffpy.srfit",
+    keywords = "optimization constraints restraints structure refinement complex modeling",
+    classifiers = [
+        # List of possible values at
+        # http://pypi.python.org/pypi?:action=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Software Development :: Libraries',
+    ],
 )
 
 if __name__ == '__main__':

diffpy/diffpy.utils

setup.py
@@ -69,38 +69,38 @@ versiondata = getversioncfg()
 
 # define distribution
 setup_args = dict(
-        name = "diffpy.utils",
-        version = versiondata.get('DEFAULT', 'version'),
-        namespace_packages = ['diffpy'],
-        packages = find_packages('src'),
-        package_dir = {'' : 'src'},
-        test_suite = 'diffpy.utils.tests',
-        include_package_data = True,
-        zip_safe = False,
-        author = 'Simon J.L. Billinge group',
-        author_email = 'sb2896@columbia.edu',
-        maintainer = 'Pavol Juhas',
-        maintainer_email = 'pavol.juhas@gmail.com',
-        description = "Shared utilities for diffpy packages.",
-        license = 'BSD-style license',
-        url = "https://github.com/diffpy/diffpy.utils/",
-        keywords = "text data parsers wx grid",
-        classifiers = [
-            # List of possible values at
-            # http://pypi.python.org/pypi?:action=list_classifiers
-            'Development Status :: 5 - Production/Stable',
-            'Environment :: Console',
-            'Intended Audience :: Developers',
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: BSD License',
-            'Operating System :: MacOS :: MacOS X',
-            'Operating System :: Microsoft :: Windows',
-            'Operating System :: POSIX',
-            'Operating System :: Unix',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7',
-            'Topic :: Scientific/Engineering :: Physics',
-        ],
+    name = "diffpy.utils",
+    version = versiondata.get('DEFAULT', 'version'),
+    namespace_packages = ['diffpy'],
+    packages = find_packages('src'),
+    package_dir = {'' : 'src'},
+    test_suite = 'diffpy.utils.tests',
+    include_package_data = True,
+    zip_safe = False,
+    author = 'Simon J.L. Billinge group',
+    author_email = 'sb2896@columbia.edu',
+    maintainer = 'Pavol Juhas',
+    maintainer_email = 'pavol.juhas@gmail.com',
+    description = "Shared utilities for diffpy packages.",
+    license = 'BSD-style license',
+    url = "https://github.com/diffpy/diffpy.utils/",
+    keywords = "text data parsers wx grid",
+    classifiers = [
+        # List of possible values at
+        # http://pypi.python.org/pypi?:action=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Physics',
+    ],
 )
 
 if __name__ == '__main__':