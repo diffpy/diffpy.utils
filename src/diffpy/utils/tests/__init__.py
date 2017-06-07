diffpy/diffpy.srfit

TST: use automated TestCase discovery.
Avoid need to maintain a list of test modules.
Use `top_level_dir` so that codecov can match source files.
Also add `pattern` argument to testsuite() for filtering test cases.

src/diffpy/srfit/tests/__init__.py
@@ -16,54 +16,61 @@
 """Unit tests for diffpy.srfit.
 """
 
-# create logger instance for the tests subpackage
+import unittest
 import logging
 
+# create logger instance for the tests subpackage
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 del logging
 
 
-def testsuite():
-    '''Build a unit tests suite for the diffpy.srfit package.
+def testsuite(pattern=''):
+    '''Create a unit tests suite for diffpy.srfit package.
 
-    Return a unittest.TestSuite object.
+    Parameters
+    ----------
+    pattern : str, optional
+        Regular expression pattern for selecting test cases.
+        Select all tests when empty.
+
+    Returns
+    -------
+    suite : `unittest.TestSuite`
+        The TestSuite object containing the matching tests.
     '''
-    import unittest
-    modulenames = '''
-        diffpy.srfit.tests.testbuilder
-        diffpy.srfit.tests.testcharacteristicfunctions
-        diffpy.srfit.tests.testconstraint
-        diffpy.srfit.tests.testcontribution
-        diffpy.srfit.tests.testdiffpyparset
-        diffpy.srfit.tests.testequation
-        diffpy.srfit.tests.testfitrecipe
-        diffpy.srfit.tests.testfitresults
-        diffpy.srfit.tests.testliterals
-        diffpy.srfit.tests.testobjcrystparset
-        diffpy.srfit.tests.testparameter
-        diffpy.srfit.tests.testparameterset
-        diffpy.srfit.tests.testpdf
-        diffpy.srfit.tests.testprofile
-        diffpy.srfit.tests.testprofilegenerator
-        diffpy.srfit.tests.testrecipeorganizer
-        diffpy.srfit.tests.testrestraint
-        diffpy.srfit.tests.testsas
-        diffpy.srfit.tests.testsgconstraints
-        diffpy.srfit.tests.testtagmanager
-        diffpy.srfit.tests.testvisitors
-        diffpy.srfit.tests.testweakrefcallable
-    '''.split()
+    import re
+    from os.path import dirname
+    from itertools import chain
+    from pkg_resources import resource_filename
     loader = unittest.defaultTestLoader
-    suite = loader.loadTestsFromNames(modulenames)
+    thisdir = resource_filename(__name__, '')
+    depth = __name__.count('.') + 1
+    topdir = thisdir
+    for i in range(depth):
+        topdir = dirname(topdir)
+    suite_all = loader.discover(thisdir, top_level_dir=topdir)
+    # always filter the suite by pattern to test-cover the selection code.
+    suite = unittest.TestSuite()
+    rx = re.compile(pattern)
+    tcases = chain.from_iterable(chain.from_iterable(suite_all))
+    for tc in tcases:
+        tcwords = tc.id().rsplit('.', 2)
+        shortname = '.'.join(tcwords[-2:])
+        if rx.search(shortname):
+            suite.addTest(tc)
+    # verify all tests are found for an empty pattern.
+    assert pattern or suite_all.countTestCases() == suite.countTestCases()
     return suite
 
 
 def test():
     '''Execute all unit tests for the diffpy.srfit package.
-    Return a unittest TestResult object.
+
+    Returns
+    -------
+    result : `unittest.TestResult`
     '''
-    import unittest
     suite = testsuite()
     runner = unittest.TextTestRunner()
     result = runner.run(suite)

diffpy/diffpy.utils

src/diffpy/utils/tests/__init__.py
@@ -16,29 +16,55 @@
 """Unit tests for diffpy.utils.
 """
 
-def testsuite():
-    '''Build a unit tests suite for the diffpy.utils package.
+import unittest
 
-    Return a unittest.TestSuite object.
+
+def testsuite(pattern=''):
+    '''Create a unit tests suite for diffpy.utils package.
+
+    Parameters
+    ----------
+    pattern : str, optional
+        Regular expression pattern for selecting test cases.
+        Select all tests when empty.
+
+    Returns
+    -------
+    suite : `unittest.TestSuite`
+        The TestSuite object containing the matching tests.
     '''
-    import unittest
-    modulenames = '''
-        diffpy.utils.tests.testloaddata
-    '''.split()
-    suite = unittest.TestSuite()
+    import re
+    from os.path import dirname
+    from itertools import chain
+    from pkg_resources import resource_filename
     loader = unittest.defaultTestLoader
-    mobj = None
-    for mname in modulenames:
-        exec ('import %s as mobj' % mname)
-        suite.addTests(loader.loadTestsFromModule(mobj))
+    thisdir = resource_filename(__name__, '')
+    depth = __name__.count('.') + 1
+    topdir = thisdir
+    for i in range(depth):
+        topdir = dirname(topdir)
+    suite_all = loader.discover(thisdir, top_level_dir=topdir)
+    # always filter the suite by pattern to test-cover the selection code.
+    suite = unittest.TestSuite()
+    rx = re.compile(pattern)
+    tcases = chain.from_iterable(chain.from_iterable(suite_all))
+    for tc in tcases:
+        tcwords = tc.id().rsplit('.', 2)
+        shortname = '.'.join(tcwords[-2:])
+        if rx.search(shortname):
+            suite.addTest(tc)
+    # verify all tests are found for an empty pattern.
+    assert pattern or suite_all.countTestCases() == suite.countTestCases()
     return suite
 
 
 def test():
     '''Execute all unit tests for the diffpy.utils package.
-    Return a unittest TestResult object.
+
+    Returns
+    -------
+    result : `unittest.TestResult`
     '''
-    import unittest
     suite = testsuite()
     runner = unittest.TextTestRunner()
     result = runner.run(suite)