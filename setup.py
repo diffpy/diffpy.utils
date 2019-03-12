4503c5bd8633b6075bf966c82e977de1d8711367
Pavol Juhas <pavol.juhas@gmail.com>
2019-03-12 01:12:50 -0400

    REL: declare compatibility with Python 3.7
    Add travis tests with Python 3.7.

--- setup.py
+++ setup.py
@@ -114,6 +114,7 @@ setup_args = dict(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering :: Physics',
     ],
 )
