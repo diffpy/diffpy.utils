78c4c99ddeab783c8697c44232f1bda14b6f122b
Andrew Yang <ay2546@columbia.edu>
2023-07-27 23:27:13 -0400

    Add preliminary preprocessing function

---
+++ src/diffpy/utils/pdfitc_helpers/struming_helpers.py
@@ -0,0 +1,35 @@
+def strumining_processing(sminputs: dict):
+    # preprocessing
+    # FIXME: make lowercase and check string inputting
+
+    # ensure stype either x-ray or neutron
+    if "mode" in sminputs:
+        stype = sminputs.pop("mode")
+        if stype.lower in ["xray", "x-ray", "x"]:
+            stype = "X"
+        elif stype.lower in ["neutron", "n"]:
+            stype = "N"
+        sminputs.update({"stype": stype})
+    if "stype" not in sminputs:
+        raise Exception("Cannot find stype.")
+
+    # ensure structure given
+    if "composition" in sminputs:
+        stru_str = sminputs.pop("composition")
+        sminputs.update({"stru_str": stru_str})
+    if "stru_str" not in sminputs:
+        raise Exception("Cannot find stru_str.")
+
+    # set default values
+    defaults = {"rmin": 1.5, "rmax": 20, "qmin": 0, "qmax": 20, "spd": 0,
+                "refinepos_option": "n", "mpi_option": "n", "db_option": "cod", "max_num": 1000}
+    stype = sminputs.get("stype")
+    if stype == "N":
+        defaults.update({"qdamp": 0.02, "qbroad": 0.02})
+    elif stype == "X":
+        defaults.update({"qdamp": 0.04, "qbroad": 0.01})
+    else:
+        raise Exception("Improper stype.")
+    for key in defaults.keys():
+        if key not in sminputs:
+            sminputs.update({key: defaults.get(key)})
