783b3d14682c9ec0a85e57bac5da778db7a5ce7c
yucongalicechen <yucong0821@g.ucla.edu>
2024-05-22 13:49:22 -0400

    added tests

--- src/diffpy/utils/tests/test_diffraction_objects.py
+++ src/diffpy/utils/tests/test_diffraction_objects.py
@@ -1,6 +1,46 @@
 import numpy as np
 from pathlib import Path
 from diffpy.utils.scattering_objects.diffraction_objects import Diffraction_object
+import pytest
+
+
+params = [
+	(["", None, "", [], {}], ["", None, "", [], {}], True),	# empty
+	(["test", 0.71, "", [[1,2], [3,4]], {"thing1": 1, "thing2": "thing2"}],
+	 ["test", 0.71, "", [[1,2], [3,4]], {"thing2": "thing2", "thing1": 1}],
+	 True),		# Compare same attributes
+	(["test1", 0.71, "", [[1,2], [3,4]], {"thing1": 1, "thing2": "thing2"}],
+	 ["test2", 0.71, "", [[1,2], [3,4]], {"thing1": 1, "thing2": "thing2"}],
+	 False),	# Different names
+	(["test", 0.71, "", [[1, 2], [3, 4]], {"thing1": 1, "thing2": "thing2"}],
+	 ["test", 1.54, "", [[1, 2], [3, 4]], {"thing1": 1, "thing2": "thing2"}],
+	 False),	# Different wavelengths
+	(["test", 0.71, "", [[1, 2], [3, 4]], {"thing1": 1, "thing2": "thing2"}],
+	 ["test", 0.71, "x-ray", [[1, 2], [3, 4]], {"thing1": 1, "thing2": "thing2"}],
+	 False),	# Different scat_quantity
+	(["test", 0.71, "", [[1, 2], [3, 4]], {"thing1": 1, "thing2": "thing2"}],
+	 ["test", 0.71, "", [[1, 3], [3, 4]], {"thing1": 1, "thing2": "thing2"}],
+	 False),	# Different on_q, on_tth, on_d
+	(["test", 0.71, "", [[1, 2], [3, 4]], {"thing1": 0, "thing2": "thing2"}],
+	 ["test", 0.71, "", [[1, 2], [3, 4]], {"thing1": 1, "thing2": "thing2"}],
+	 False),	# Different metadata
+]
+
+@pytest.mark.parametrize("inputs1, inputs2, expected", params)
+def test_diffraction_objects_equality(inputs1, inputs2, expected):
+	diffraction_object1 = Diffraction_object(name=inputs1[0], wavelength=inputs1[1])
+	diffraction_object2 = Diffraction_object(name=inputs2[0], wavelength=inputs2[1])
+	diffraction_object1.scat_quantity = inputs1[2]
+	diffraction_object2.scat_quantity = inputs2[2]
+	diffraction_object1.on_q = inputs1[3]
+	diffraction_object2.on_q = inputs2[3]
+	diffraction_object1.on_tth = inputs1[3]
+	diffraction_object2.on_tth = inputs2[3]
+	diffraction_object1.on_d = inputs1[3]
+	diffraction_object2.on_d = inputs2[3]
+	diffraction_object1.metadata = inputs1[4]
+	diffraction_object2.metadata = inputs2[4]
+	assert (diffraction_object1 == diffraction_object2) == expected
 
 def test_dump(tmp_path):
 	x, y = np.linspace(0, 10, 11), np.linspace(0, 10, 11)
