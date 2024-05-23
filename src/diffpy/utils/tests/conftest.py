c3ebf94e166b7bb2879bef1235bd5033683d5bf1
yucongalicechen <yucong0821@g.ucla.edu>
2024-05-23 17:58:42 -0400

    reformatted tests and added error messages for invalid conf files

--- src/diffpy/utils/tests/conftest.py
+++ src/diffpy/utils/tests/conftest.py
@@ -11,14 +11,9 @@ def user_filesystem(tmp_path):
     home_dir.mkdir(parents=True, exist_ok=True)
     cwd_dir = base_dir / "cwd_dir"
     cwd_dir.mkdir(parents=True, exist_ok=True)
-    new_dir = base_dir / "new_dir"
-    new_dir.mkdir(parents=True, exist_ok=True)
 
     home_config_data = {"username": "home_username", "email": "home@email.com"}
-    cwd_config_data = {"username": "cwd_username", "email": "cwd@email.com"}
     with open(home_dir / "diffpyconfig.json", "w") as f:
         json.dump(home_config_data, f)
-    with open(cwd_dir / "diffpyconfig.json", "w") as f:
-        json.dump(cwd_config_data, f)
 
     yield tmp_path
--- src/diffpy/utils/tests/test_tools.py
+++ src/diffpy/utils/tests/test_tools.py
@@ -7,76 +7,104 @@ import pytest
 
 from diffpy.utils.scattering_objects.tools import get_user_info
 
-# Helper test function
-def _test_user_info(monkeypatch, cwd, inputs, expected, user_filesystem, no_conf_file=False, expect_error=False, error_msg=None):
-    # Determine paths
-    cwd_path = user_filesystem / cwd
-    home_dir = user_filesystem / "home_dir"
-    cwd_config_path = cwd_path / "diffpyconfig.json"
-    home_config_path = home_dir / "diffpyconfig.json"
-
-    # Set monkeypatch attributes
-    input_user = iter(inputs)
-    monkeypatch.setattr("builtins.input", lambda _: next(input_user))
-    monkeypatch.setattr("diffpy.utils.scattering_objects.user_config.CWD_CONFIG_PATH", cwd_config_path)
-    monkeypatch.setattr("diffpy.utils.scattering_objects.user_config.HOME_CONFIG_PATH", home_config_path)
-
-    # Handle no configuration file
-    if no_conf_file:
-        os.remove(home_dir / "diffpyconfig.json")
-    # Change directory to specified cwd
-    os.chdir(user_filesystem / cwd)
-
-    # Perform the test
-    if expect_error:
-        with pytest.raises(ValueError, match=error_msg):
-            get_user_info()
-    else:
-        actual_username, actual_email = get_user_info()
-        expected_username, expected_email, expected_conf_folder, expected_conf_username, expected_conf_email = expected
-        assert actual_username == expected_username
-        assert actual_email == expected_email
-
-        expected_conf_path = (user_filesystem / expected_conf_folder) / "diffpyconfig.json"
-        with open(expected_conf_path, "r") as f:
-            config_data = json.load(f)
-            assert config_data == {"username": expected_conf_username, "email": expected_conf_email}
 
 params_user_info_with_conf_file = [
-    # No cwd config file, only home config file
-    ("new_dir", ["", ""], ["home_username", "home@email.com", "home_dir", "home_username", "home@email.com"]),
-    ("new_dir", ["input_username", ""], ["input_username", "home@email.com", "home_dir", "home_username", "home@email.com"]),
-    ("new_dir", ["", "input@email.com"], ["home_username", "input@email.com", "home_dir", "home_username", "home@email.com"]),
-    ("new_dir", ["input_username", "input@email.com"],
-     ["input_username", "input@email.com", "home_dir", "home_username", "home@email.com"]),
-
-    # There exists valid cwd config file
-    ("cwd_dir", ["", ""], ["cwd_username", "cwd@email.com", "cwd_dir", "cwd_username", "cwd@email.com"]),
-    ("cwd_dir", ["input_username", ""], ["input_username", "cwd@email.com", "cwd_dir", "cwd_username", "cwd@email.com"]),
-    ("cwd_dir", ["", "input@email.com"], ["cwd_username", "input@email.com", "cwd_dir", "cwd_username", "cwd@email.com"]),
-    ("cwd_dir", ["input_username", "input@email.com"],
-     ["input_username", "input@email.com", "cwd_dir", "cwd_username", "cwd@email.com"]),
+    (["", ""], ["home_username", "home@email.com", "cwd_username", "cwd@email.com"]),
+    (["input_username", ""], ["input_username", "home@email.com", "input_username", "cwd@email.com"]),
+    (["", "input@email.com"], ["home_username", "input@email.com", "cwd_username", "input@email.com"]),
+    (["input_username", "input@email.com"], ["input_username", "input@email.com", "input_username", "input@email.com"]),
 ]
+@pytest.mark.parametrize("inputs, expected", params_user_info_with_conf_file)
+def test_load_user_info_with_conf_file(monkeypatch, inputs, expected, user_filesystem):
+    expected_username_home, expected_email_home, expected_username_cwd, expected_email_cwd = expected
+    home_dir = user_filesystem / "home_dir"
+    cwd_dir = user_filesystem / "cwd_dir"
+
+    # conf file is in home directory, not in cwd
+    input_user = iter(inputs)
+    monkeypatch.setattr("builtins.input", lambda _: next(input_user))
+    monkeypatch.setattr("diffpy.utils.scattering_objects.user_config.CWD_CONFIG_PATH", cwd_dir / "diffpyconfig.json")
+    monkeypatch.setattr("diffpy.utils.scattering_objects.user_config.HOME_CONFIG_PATH", home_dir / "diffpyconfig.json")
+    actual_username, actual_email = get_user_info()
+    assert actual_username == expected_username_home
+    assert actual_email == expected_email_home
+
+    # conf file is in cwd
+    cwd_username, cwd_email = "cwd_username", "cwd@email.com"
+    with open(cwd_dir / "diffpyconfig.json", "w") as f:
+        json.dump({"username": cwd_username, "email": cwd_email}, f)
+    input_user = iter(inputs)
+    monkeypatch.setattr("builtins.input", lambda _: next(input_user))
+    actual_username, actual_email = get_user_info()
+    assert actual_username == expected_username_cwd
+    assert actual_email == expected_email_cwd
+
 
 params_user_info_without_conf_file = [
-    ("new_dir", ["input_username", "input@email.com"], ["input_username", "input@email.com", "home_dir", "input_username", "input@email.com"]),
+    (["new_username", "new@email.com"], ["new_username", "new@email.com", "new_username", "new@email.com"]),
 ]
+@pytest.mark.parametrize("inputs, expected", params_user_info_without_conf_file)
+def test_load_user_info_without_conf_file(monkeypatch, inputs, expected, user_filesystem):
+    expected_username, expected_email, expected_conf_username, expected_conf_email = expected
+    home_dir = user_filesystem / "home_dir"
+    cwd_dir = user_filesystem / "cwd_dir"
+    os.remove(home_dir / "diffpyconfig.json")
+    input_user = iter(inputs)
+    monkeypatch.setattr("builtins.input", lambda _: next(input_user))
+    monkeypatch.setattr("diffpy.utils.scattering_objects.user_config.CWD_CONFIG_PATH", cwd_dir / "diffpyconfig.json")
+    monkeypatch.setattr("diffpy.utils.scattering_objects.user_config.HOME_CONFIG_PATH", user_filesystem / "diffpyconfig.json")
+
+    actual_username, actual_email = get_user_info()
+    assert actual_username == expected_username
+    assert actual_email == expected_email
+    with open(user_filesystem / "diffpyconfig.json", "r") as f:
+        config_data = json.load(f)
+        assert config_data == {"username": expected_conf_username, "email": expected_conf_email}
+
 
 params_user_info_bad = [
-    ("new_dir", ["", ""], "Please rerun the program and provide a username and email."),
-    ("new_dir", ["", "input@email.com"], "Please rerun the program and provide a username."),
-    ("new_dir", ["input_username", ""], "Please rerun the program and provide an email."),
-    ("new_dir", ["input_username", "bad_email"], "Please rerun the program and provide a valid email."),
+    # Invalid inputs
+    (["input_username", "bad_email"], "Please rerun the program and provide a valid email."),
+    # Skipped inputs when there is no valid conf file
+    (["", ""], "Please rerun the program and provide a username and email."),
+    (["", "input@email.com"], "Please rerun the program and provide a username."),
+    (["input_username", ""], "Please rerun the program and provide an email."),
 ]
+@pytest.mark.parametrize("inputs, msg", params_user_info_bad)
+def test_load_user_info_bad(monkeypatch, inputs, msg, user_filesystem):
+    home_dir = user_filesystem / "home_dir"
+    cwd_dir = user_filesystem / "cwd_dir"
+    os.remove(home_dir / "diffpyconfig.json")
+    input_user = iter(inputs)
+    monkeypatch.setattr("builtins.input", lambda _: next(input_user))
+    monkeypatch.setattr("diffpy.utils.scattering_objects.user_config.CWD_CONFIG_PATH", cwd_dir / "diffpyconfig.json")
+    monkeypatch.setattr("diffpy.utils.scattering_objects.user_config.HOME_CONFIG_PATH", home_dir / "diffpyconfig.json")
+    with pytest.raises(ValueError, match=msg[0]):
+        get_user_info()
 
-@pytest.mark.parametrize("cwd, inputs, expected", params_user_info_with_conf_file)
-def test_load_user_info_with_conf_file(monkeypatch, cwd, inputs, expected, user_filesystem):
-    _test_user_info(monkeypatch, cwd, inputs, expected, user_filesystem)
 
-@pytest.mark.parametrize("cwd, inputs, expected", params_user_info_without_conf_file)
-def test_load_user_info_without_conf_file(monkeypatch, cwd, inputs, expected, user_filesystem):
-    _test_user_info(monkeypatch, cwd, inputs, expected, user_filesystem, no_conf_file=True)
+params_user_info_invalid_conf_files = [
+    # Invalid conf files
+    # Test first invalid home conf file, then invalid cwd conf file
+    (["input_username", "input@email.com"], "Please provide a configuration file with username and email."),
+]
+@pytest.mark.parametrize("inputs, msg", params_user_info_invalid_conf_files)
+def test_load_user_info_invalid_conf_files(monkeypatch, inputs, msg, user_filesystem):
+    home_dir = user_filesystem / "home_dir"
+    cwd_dir = user_filesystem / "cwd_dir"
+    os.remove(home_dir / "diffpyconfig.json")
+    with open(home_dir / "diffpyconfig.json", "w") as f:
+        json.dump({}, f)
+    input_user = iter(inputs)
+    monkeypatch.setattr("builtins.input", lambda _: next(input_user))
+    monkeypatch.setattr("diffpy.utils.scattering_objects.user_config.CWD_CONFIG_PATH", cwd_dir / "diffpyconfig.json")
+    monkeypatch.setattr("diffpy.utils.scattering_objects.user_config.HOME_CONFIG_PATH", home_dir / "diffpyconfig.json")
+    with pytest.raises(ValueError, match=msg[0]):
+        get_user_info()
 
-@pytest.mark.parametrize("cwd, inputs, msg", params_user_info_bad)
-def test_load_user_info_bad(monkeypatch, cwd, inputs, msg, user_filesystem):
-    _test_user_info(monkeypatch, cwd, inputs, None, user_filesystem, no_conf_file=True, expect_error=True, error_msg=msg)
+    with open(cwd_dir / "diffpyconfig.json", "w") as f:
+        json.dump({}, f)
+    input_user = iter(inputs)
+    monkeypatch.setattr("builtins.input", lambda _: next(input_user))
+    with pytest.raises(ValueError, match=msg[0]):
+        get_user_info()
