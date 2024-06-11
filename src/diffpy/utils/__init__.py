35ea0dff71de96d33c5a02bb9dd71ffd7a8d7b1d
yucongalicechen <yucong0821@g.ucla.edu>
2024-06-11 13:52:57 -0400

    add a tools variable

--- src/diffpy/utils/__init__.py
+++ src/diffpy/utils/__init__.py
@@ -21,7 +21,13 @@ from diffpy.utils.tools import get_package_info, get_user_info
 # package version
 from diffpy.utils.version import __version__
 
+__tools__ = [
+  get_package_info,
+  get_user_info,
+]
+
 # silence the pyflakes syntax checker
 assert __version__ or True
+assert __tools__ or True
 
 # End of file
