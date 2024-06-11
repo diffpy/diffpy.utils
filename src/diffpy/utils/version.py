1ff119c6c5b35beb0021caddc121fc7cfe8aa4c7
Simon Billinge <sbillinge@users.noreply.github.com>
2024-06-10 22:00:17 -0400

    added whitespace to trigger a build

--- src/diffpy/utils/version.py
+++ src/diffpy/utils/version.py
@@ -15,8 +15,8 @@
 
 """Definition of __version__."""
 
-# We do not use the other three variables, but can be added back if needed.
-# __all__ = ["__date__", "__git_commit__", "__timestamp__", "__version__"]
+#  We do not use the other three variables, but can be added back if needed.
+#  __all__ = ["__date__", "__git_commit__", "__timestamp__", "__version__"]
 
 # obtain version information
 from importlib.metadata import version
