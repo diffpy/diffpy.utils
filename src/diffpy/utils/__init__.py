f70eaa0ada15b4fa610571177b19be627fcf87e2
Caden Myers <cjm2304@columbia.edu>
2025-06-10 17:16:56 -0400

    skpkg: small modifications to files in src

--- src/diffpy/utils/__init__.py
+++ src/diffpy/utils/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 ##############################################################################
 #
-# (c) 2024 The Trustees of Columbia University in the City of New York.
+# (c) 2025 The Trustees of Columbia University in the City of New York.
 # All rights reserved.
 #
-# File coded by: Billinge Group members and community contributors.
+# File coded by: Simon Billinge, Billinge Group members.
 #
 # See GitHub contributions for a more detailed list of contributors.
 # https://github.com/diffpy/diffpy.utils/graphs/contributors
@@ -19,3 +19,5 @@ from diffpy.utils.version import __version__
 
 # silence the pyflakes syntax checker
 assert __version__ or True
+
+# End of file
